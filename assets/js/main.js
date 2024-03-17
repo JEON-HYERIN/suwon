fc2bcdcb597e4879388f7b79a82305ab24eec608
jeonhyerin <jeonhyelin19951118@gmail.com>
2024-03-18 04:17:38 +0900

    Fix: 푸터 탭영역 마크업

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,34 @@
 
 
-let gnbHeight = $('.group-nav .common-inner').outerHeight();
+let gnbHeight = $('.nav-container .common-inner').outerHeight();
 let lnbHeight = 0;
 
 $('.link-gnb').on('mouseenter', function(e) {
   e.preventDefault();
-  lnbHeight = $(this).parent().find('.lnb-list-container').outerHeight();
-  $('.lnb-list-container').css({
+  lnbHeight = $(this).parent().find('.lnb-list-wrap').outerHeight();
+  $('.nav-container').css({
     'height': (gnbHeight + lnbHeight) + "px"
   });
-  $('.group-nav').addClass('is-open');
+  $('.nav-container').addClass('is-open');
   $(this).parent().addClass('is-active').siblings().removeClass('is-active');
 })
 
-$('.lnb-list-container').on('mouseleave', function(e) {
+$('.lnb-list-wrap').on('mouseleave', function(e) {
   e.preventDefault();
-  $('.lnb-list-container').removeAttr('style');
-  $('.group-nav').removeClass('is-open');
+  $('.nav-container').removeAttr('style');
+  $('.nav-container').removeClass('is-open');
   $('.gnb-item').removeClass('is-active');
 })
 
+$('.footer .btn-corp').unbind('click').bind('click', function() {
+  const index = $(this).parent().index();
+
+  if($(this).parent().hasClass('is-active')) {
+    $('.footer .site-list-wrap').stop().slideUp();
+    $(this).parent().removeClass('is-active');
+  } else {
+    $('.footer .site-list-wrap').stop().slideUp();
+    $('.footer .site-list-wrap').eq(index).stop().slideDown();
+    $(this).parent().addClass('is-active').siblings().removeClass('is-active');
+  }
+});
