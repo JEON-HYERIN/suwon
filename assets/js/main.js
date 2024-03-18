d166e5225a4c31aa4b1e40ff5c320424cd7abf52
jeonhyerin <jeonhyelin19951118@gmail.com>
2024-03-18 16:03:51 +0900

    Fix: 메인비주얼 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 
+// a태그 기본동작 방지
+$(function () {
+  $(document).on('click', 'a[href="#"]', function (e) {
+    e.preventDefault();
+  });
+})
 
 let gnbHeight = $('.nav-container .common-inner').outerHeight();
 let lnbHeight = 0;
@@ -32,3 +38,46 @@ $('.footer .btn-related').unbind('click').bind('click', function() {
     $(this).parent().addClass('is-active').siblings().removeClass('is-active');
   }
 });
+
+const visualSlide = $('.section-visual .slide-wrapper'); 
+visualSlide.slick({
+  autoplay : true,
+  infinite : true,
+  prevArrow: $('.btn-prev'),
+  nextArrow: $('.btn-next'),
+  // dots: true,
+  // dotsClass: 'page',
+  // customPaging: function (slider, i) {
+
+  //   const currentPage = String((i + 1)).padStart(2, '0')
+  //   const totalPage = String(slider.slideCount).padStart(2,'0');
+    
+  //   return  '<span class="current">'+ currentPage +'</span><span class="gap">-</span><span class="total">' + totalPage + '</span>';
+  // }
+})
+visualSlide.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){ 
+
+  const currentPage = String((currentSlide ? currentSlide : 0) + 1).padStart(2, '0')
+  const totalPage = String(slick.slideCount).padStart(2,'0');
+ 
+  // $('.section-visual .page').html("<span class='current'>"+currentPage+"</span><span> - " + "</span>" + "<span class='total'>" + totalPage +"</span>");
+
+  $('.section-visual .page .current').text(currentPage);
+  $('.section-visual .page .total').text(totalPage);
+  });
+
+  let slidePaused = false; //자동재생상태
+  $('.section-visual .btn-control').on('click', function() {
+    // 정지상태라면
+    if(slidePaused) {
+      $('.btn-control').removeClass('pause');
+      visualSlide.slick('slickPlay');
+      $('.btn-control .blind').text('정지');
+      slidePaused = false;
+    } else {
+      $('.btn-control').addClass('pause');
+      visualSlide.slick('slickPause');
+      $('.btn-control .blind').text('재생');
+      slidePaused = true;
+    }
+  });
